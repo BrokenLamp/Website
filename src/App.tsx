fed98d8a9e4ea0c7ac2d2423e5e3de62ba0d73de
Carter Wagner <cjwwcw@umsystem.edu>
2020-03-31 22:57:25 -0500

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled, { AnyStyledComponent } from "styled-components";
+import Nav from "./pages/Nav";
+
+const Layout: AnyStyledComponent = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: 20% 70% 10%;
+  grid-template-areas:
+    "h h h h h h h h h h h h"
+    "c c c c c c c c c c c c";
+    "f f f f f f f f f f f f";
+`;
+const Header: AnyStyledComponent = styled.div`
+  grid-area: h;
+`;
+const Content: AnyStyledComponent = styled.div`
+  grid-area: c;
+`;
+const Footer: AnyStyledComponent = styled.div`
+  grid-area: f;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <Nav />
+      </Header>
+      <Content></Content>
+      <Footer></Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Nav.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styled, { AnyStyledComponent } from "styled-components";
+
+const Navone: AnyStyledComponent = styled.nav`
+  background-color: #000;
+  height: 90px;
+  width: 100%;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+`;
+const List: AnyStyledComponent = styled.li`
+  padding: 20px;
+  list-style-type: none;
+`;
+const Center: AnyStyledComponent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 40px;
+`;
+const Bltext: AnyStyledComponent = styled.div`
+  font-size: 24px;
+  font-family: "Poppins", sans-serif;
+  color: #ffd966;
+  font-weight: bold;
+  border-bottom: 2px solid transparent;
+
+  &:hover {
+    border-bottom-color: #ffd966;
+  }
+  transition: all 0.1s ease-in-out;
+`;
+const Overview: AnyStyledComponent = styled.div`
+  flex: 1;
+  display: flex;
+  height: 100%;
+`;
+
+const Link: AnyStyledComponent = styled.a`
+  text-decoration: none;
+  color: #ffe8a1;
+  font-family: "Poppins", sans-serif;
+
+  border-bottom: 2px solid transparent;
+
+  &:hover {
+    border-bottom-color: #ffe8a1;
+  }
+  transition: all 0.2s ease-in-out;
+`;
+const Constraint: AnyStyledComponent = styled.div`
+  display: flex;
+  height: 100%;
+  max-width: 1200px;
+  margin: auto;
+`;
+const Nav: React.FC = () => {
+  return (
+    <Navone>
+      <Constraint>
+        <Overview>
+          <Center>
+            <List>
+              <Link href="https://www.google.com/">
+                <Bltext>Broken Lamp</Bltext>
+              </Link>
+            </List>
+
+            <List>
+              <Link href="https://www.google.com/">Software</Link>
+            </List>
+
+            <List>
+              <Link href="https://www.google.com/">Games</Link>
+            </List>
+            <List>
+              <Link href="https://www.google.com/">Websites</Link>
+            </List>
+          </Center>
+        </Overview>
+      </Constraint>
+    </Navone>
+  );
+};
+
+export default Nav;
